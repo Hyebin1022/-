1f54e13c0e9bd12cdda6843ef2778f41723f2cb5
Shin Hyebin <141225159+Hyebin1022@users.noreply.github.com>
2023-11-11 18:59:04 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/README.md
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 문자열이 몇 번 등장하는지 세기 - 181871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.3 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 18:54:8
+2023년 11월 6일 18:59:7
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,12 +1,6 @@
-def solution(myString, pat):    
-    start = 0
-    cnt = 0
-    
-    while True:
-        idx = myString.find(pat, start)
-        if idx == -1:
-            break
-        cnt += 1
-        start = idx + 1
-    
-    return cnt
+def solution(myString, pat):
+    answer = 0
+    for i, x in enumerate(myString) :
+        if myString[i:].startswith(pat) :
+            answer += 1
+    return answer
