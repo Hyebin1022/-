a0932c334b3ac114c3285d8239b9650d1b1d6dd7
Shin Hyebin <141225159+Hyebin1022@users.noreply.github.com>
2023-11-20 08:33:10 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,9 +1,5 @@
 def solution(a, b):
     answer = 0
-    if a%2==1 and b%2==1:
-        answer = a**2 + b**2
-    elif a%2==1 or b%2==1:
-        answer = 2*(a+b)
-    else:
-        answer = abs(a-b)
-    return answer
+    if a%2==1 and b%2==1: return a**2 + b**2
+    elif a%2==1 or b%2==1: return 2*(a+b)
+    return abs(a-b)
